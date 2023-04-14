cd673069ab764dd9d649a0223abe43f78ecb69f6
vic-en <31972210+vic-en@users.noreply.github.com>
2023-04-14 15:49:48 -0500

    Update hummingbot/connector/derivative/phemex_perpetual/phemex_perpetual_api_order_book_data_source.py
    Co-authored-by: Petio Petrov <petioptrv@icloud.com>

--- hummingbot/connector/derivative/phemex_perpetual/phemex_perpetual_api_order_book_data_source.py
+++ hummingbot/connector/derivative/phemex_perpetual/phemex_perpetual_api_order_book_data_source.py
@@ -23,9 +23,6 @@ TradeStructure = namedtuple("Trade", "timestamp side price amount")
 
 
 class PhemexPerpetualAPIOrderBookDataSource(PerpetualAPIOrderBookDataSource):
-    _bpobds_logger: Optional[HummingbotLogger] = None
-    _trading_pair_symbol_map: Dict[str, Mapping[str, str]] = {}
-    _mapping_initialization_lock = asyncio.Lock()
     _index_price_index: int
     _mark_price_index: int
     _symbol_index: int
