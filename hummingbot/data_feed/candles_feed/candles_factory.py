8631539549e938deba390ce79ad8458a4bca87d6
bczhang <zhangbocheng@zhangboengdembp>
2023-07-11 18:31:27 +0800

    format code

--- hummingbot/data_feed/candles_feed/candles_factory.py
+++ hummingbot/data_feed/candles_feed/candles_factory.py
@@ -4,7 +4,6 @@ from hummingbot.data_feed.candles_feed.binance_spot_candles import BinanceSpotCa
 from hummingbot.data_feed.candles_feed.gate_io_perpetual_candles import GateioPerpetualCandles
 from hummingbot.data_feed.candles_feed.gate_io_spot_candles import GateioSpotCandles
 from hummingbot.data_feed.candles_feed.kucoin_spot_candles.kucoin_spot_candles import KucoinSpotCandles
-from hummingbot.data_feed.candles_feed.ascend_ex_spot_candles.ascend_ex_spot_candles import AscendExSpotCandles
 
 
 class CandlesFactory:
