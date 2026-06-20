75a7edbd5652aa8e438d04a6761f8429867005ed
MementoRC <claude.rc@gmail.com>
2026-06-19 21:17:36 -0500

    feat(kraken): conditional order routing for STOP_LOSS/TAKE_PROFIT/TRAILING_STOP

--- hummingbot/connector/exchange/kraken/kraken_auth.py
+++ hummingbot/connector/exchange/kraken/kraken_auth.py
@@ -6,6 +6,7 @@ import hmac
 import json
 import time
 from typing import Any
+import urllib
 from urllib.parse import urlparse
 
 from hummingbot.connector.time_synchronizer import TimeSynchronizer
@@ -22,13 +23,12 @@ class KrakenAuth(AuthBase):
         self.time_provider = time_provider
 
     @classmethod
-    def get_tracking_nonce(self) -> str:
-        nonce = int(time.time())
-        self._last_tracking_nonce = nonce if nonce > self._last_tracking_nonce else self._last_tracking_nonce + 1
-        return str(self._last_tracking_nonce)
+    def get_tracking_nonce(cls) -> str:
+        nonce = int(time.time() * 1000)
+        cls._last_tracking_nonce = nonce if nonce > cls._last_tracking_nonce else cls._last_tracking_nonce + 1
+        return str(cls._last_tracking_nonce)
 
     async def rest_authenticate(self, request: RESTRequest) -> RESTRequest:
-
         data = json.loads(request.data) if request.data is not None else {}
         _path = urlparse(request.url).path
 
@@ -56,11 +56,13 @@ class KrakenAuth(AuthBase):
         # Variables (API method, nonce, and POST data)
         api_path: bytes = bytes(uri, "utf-8")
         api_nonce: str = self.get_tracking_nonce()
-        api_post: str = "nonce=" + api_nonce
+        api_post: str = f"nonce={api_nonce}"
 
         if data is not None:
             for key, value in data.items():
-                api_post += f"&{key}={value}"
+                encoded_key = urllib.parse.quote(str(key))
+                encoded_value = urllib.parse.quote(str(value))
+                api_post += f"&{encoded_key}={encoded_value}"
 
         # Cryptographic hash algorithms
         api_sha256: bytes = hashlib.sha256(bytes(api_nonce + api_post, "utf-8")).digest()
