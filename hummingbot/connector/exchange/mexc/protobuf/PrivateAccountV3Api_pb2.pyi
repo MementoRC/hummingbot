48a1981686bcc68c7959e6b9e5daab90c5cfb6c3
MergeSim <sim@test.local>
2026-09-09 17:29:16 -0500

    style: final ruff format + lint fix + isort pass after rebuild

--- hummingbot/connector/exchange/mexc/protobuf/PrivateAccountV3Api_pb2.pyi
+++ hummingbot/connector/exchange/mexc/protobuf/PrivateAccountV3Api_pb2.pyi
@@ -1,11 +1,20 @@
-from google.protobuf import descriptor as _descriptor
-from google.protobuf import message as _message
 from typing import ClassVar as _ClassVar, Optional as _Optional
+
+from google.protobuf import descriptor as _descriptor, message as _message
 
 DESCRIPTOR: _descriptor.FileDescriptor
 
 class PrivateAccountV3Api(_message.Message):
-    __slots__ = ("vcoinName", "coinId", "balanceAmount", "balanceAmountChange", "frozenAmount", "frozenAmountChange", "type", "time")
+    __slots__ = (
+        "vcoinName",
+        "coinId",
+        "balanceAmount",
+        "balanceAmountChange",
+        "frozenAmount",
+        "frozenAmountChange",
+        "type",
+        "time",
+    )
     VCOINNAME_FIELD_NUMBER: _ClassVar[int]
     COINID_FIELD_NUMBER: _ClassVar[int]
     BALANCEAMOUNT_FIELD_NUMBER: _ClassVar[int]
@@ -22,4 +31,14 @@ class PrivateAccountV3Api(_message.Message):
     frozenAmountChange: str
     type: str
     time: int
-    def __init__(self, vcoinName: _Optional[str] = ..., coinId: _Optional[str] = ..., balanceAmount: _Optional[str] = ..., balanceAmountChange: _Optional[str] = ..., frozenAmount: _Optional[str] = ..., frozenAmountChange: _Optional[str] = ..., type: _Optional[str] = ..., time: _Optional[int] = ...) -> None: ...
+    def __init__(
+        self,
+        vcoinName: _Optional[str] = ...,
+        coinId: _Optional[str] = ...,
+        balanceAmount: _Optional[str] = ...,
+        balanceAmountChange: _Optional[str] = ...,
+        frozenAmount: _Optional[str] = ...,
+        frozenAmountChange: _Optional[str] = ...,
+        type: _Optional[str] = ...,
+        time: _Optional[int] = ...,
+    ) -> None: ...
