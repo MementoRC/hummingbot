24cfa2dcc47e4cd4faef76993332f1347dcd53bb
MergeSim <sim@test.local>
2026-09-16 17:03:04 -0500

    chore(ci-base): py312 post-merge transform pass

--- test/hummingbot/connector/derivative/injective_v2_perpetual/test_injective_v2_perpetual_derivative_for_delegated_account.py
+++ test/hummingbot/connector/derivative/injective_v2_perpetual/test_injective_v2_perpetual_derivative_for_delegated_account.py
@@ -1,11 +1,10 @@
 import asyncio
 import base64
-import json
 from collections import OrderedDict
 from decimal import Decimal
 from functools import partial
-from test.hummingbot.connector.exchange.injective_v2.programmable_query_executor import ProgrammableQueryExecutor
-from typing import Any, Callable, Dict, List, Optional, Tuple, Union
+import json
+from typing import Any, Callable
 from unittest.mock import AsyncMock, patch
 
 from aioresponses import aioresponses
@@ -47,6 +46,7 @@ from hummingbot.core.event.events import (
 )
 from hummingbot.core.network_iterator import NetworkStatus
 from hummingbot.core.utils.async_utils import safe_gather
+from test.hummingbot.connector.exchange.injective_v2.programmable_query_executor import ProgrammableQueryExecutor
 
 
 class InjectiveV2PerpetualDerivativeTests(AbstractPerpetualDerivativeTests.PerpetualDerivativeTests):
@@ -86,7 +86,7 @@ class InjectiveV2PerpetualDerivativeTests(AbstractPerpetualDerivativeTests.Perpe
         )
         self._initialize_timeout_height_patch.start()
         super().setUp()
-        self._logs_event: Optional[asyncio.Event] = None
+        self._logs_event: asyncio.Event | None = None
         self.exchange._data_source.logger().setLevel(1)
         self.exchange._data_source.logger().addHandler(self)
 
@@ -116,7 +116,7 @@ class InjectiveV2PerpetualDerivativeTests(AbstractPerpetualDerivativeTests.Perpe
             await self._logs_event.wait()
 
     @property
-    def expected_supported_position_modes(self) -> List[PositionMode]:
+    def expected_supported_position_modes(self) -> list[PositionMode]:
         return [PositionMode.ONEWAY]
 
     @property
@@ -146,7 +146,7 @@ class InjectiveV2PerpetualDerivativeTests(AbstractPerpetualDerivativeTests.Perpe
         self,
         position_mode: PositionMode,
         mock_api: aioresponses,
-        callback: Optional[Callable] = lambda *args, **kwargs: None,
+        callback: Callable | None = lambda *args, **kwargs: None,
     ):
         raise NotImplementedError
 
@@ -154,18 +154,18 @@ class InjectiveV2PerpetualDerivativeTests(AbstractPerpetualDerivativeTests.Perpe
         self,
         position_mode: PositionMode,
         mock_api: aioresponses,
-        callback: Optional[Callable] = lambda *args, **kwargs: None,
-    ) -> Tuple[str, str]:
+        callback: Callable | None = lambda *args, **kwargs: None,
+    ) -> tuple[str, str]:
         # Do nothing
         return "", ""
 
     def configure_failed_set_leverage(
-        self, leverage: int, mock_api: aioresponses, callback: Optional[Callable] = lambda *args, **kwargs: None
-    ) -> Tuple[str, str]:
+        self, leverage: int, mock_api: aioresponses, callback: Callable | None = lambda *args, **kwargs: None
+    ) -> tuple[str, str]:
         raise NotImplementedError
 
     def configure_successful_set_leverage(
-        self, leverage: int, mock_api: aioresponses, callback: Optional[Callable] = lambda *args, **kwargs: None
+        self, leverage: int, mock_api: aioresponses, callback: Callable | None = lambda *args, **kwargs: None
     ):
         raise NotImplementedError
 
@@ -230,7 +230,7 @@ class InjectiveV2PerpetualDerivativeTests(AbstractPerpetualDerivativeTests.Perpe
         }
 
     @property
-    def all_symbols_including_invalid_pair_mock_response(self) -> Tuple[str, Any]:
+    def all_symbols_including_invalid_pair_mock_response(self) -> tuple[str, Any]:
         response = self.all_derivative_markets_mock_response
         response["invalid_market_id"] = DerivativeMarket(
             id="invalid_market_id",
@@ -446,7 +446,7 @@ class InjectiveV2PerpetualDerivativeTests(AbstractPerpetualDerivativeTests.Perpe
         return "10414162_22_33"
 
     @property
-    def all_spot_markets_mock_response(self) -> Dict[str, SpotMarket]:
+    def all_spot_markets_mock_response(self) -> dict[str, SpotMarket]:
         base_native_token = Token(
             name="Base Asset",
             symbol=self.base_asset,
@@ -485,7 +485,7 @@ class InjectiveV2PerpetualDerivativeTests(AbstractPerpetualDerivativeTests.Perpe
         return {native_market.id: native_market}
 
     @property
-    def all_derivative_markets_mock_response(self) -> Dict[str, DerivativeMarket]:
+    def all_derivative_markets_mock_response(self) -> dict[str, DerivativeMarket]:
         quote_native_token = Token(
             name="Base Asset",
             symbol=self.quote_asset,
@@ -571,7 +571,7 @@ class InjectiveV2PerpetualDerivativeTests(AbstractPerpetualDerivativeTests.Perpe
         raise NotImplementedError
 
     def configure_all_symbols_response(
-        self, mock_api: aioresponses, callback: Optional[Callable] = lambda *args, **kwargs: None
+        self, mock_api: aioresponses, callback: Callable | None = lambda *args, **kwargs: None
     ) -> str:
         all_markets_mock_response = self.all_spot_markets_mock_response
         self.exchange._data_source._query_executor._spot_markets_responses.put_nowait(all_markets_mock_response)
@@ -586,8 +586,8 @@ class InjectiveV2PerpetualDerivativeTests(AbstractPerpetualDerivativeTests.Perpe
     def configure_trading_rules_response(
         self,
         mock_api: aioresponses,
-        callback: Optional[Callable] = lambda *args, **kwargs: None,
-    ) -> List[str]:
+        callback: Callable | None = lambda *args, **kwargs: None,
+    ) -> list[str]:
 
         self.configure_all_symbols_response(mock_api=mock_api, callback=callback)
         return ""
@@ -595,8 +595,8 @@ class InjectiveV2PerpetualDerivativeTests(AbstractPerpetualDerivativeTests.Perpe
     def configure_erroneous_trading_rules_response(
         self,
         mock_api: aioresponses,
-        callback: Optional[Callable] = lambda *args, **kwargs: None,
-    ) -> List[str]:
+        callback: Callable | None = lambda *args, **kwargs: None,
+    ) -> list[str]:
 
         self.exchange._data_source._query_executor._spot_markets_responses.put_nowait({})
         response = self.trading_rules_request_erroneous_mock_response
@@ -608,7 +608,7 @@ class InjectiveV2PerpetualDerivativeTests(AbstractPerpetualDerivativeTests.Perpe
         return ""
 
     def configure_successful_cancelation_response(
-        self, order: InFlightOrder, mock_api: aioresponses, callback: Optional[Callable] = lambda *args, **kwargs: None
+        self, order: InFlightOrder, mock_api: aioresponses, callback: Callable | None = lambda *args, **kwargs: None
     ) -> str:
         transaction_simulation_response = self._msg_exec_simulation_mock_response()
         self.exchange._data_source._query_executor._simulate_transaction_responses.put_nowait(
@@ -621,7 +621,7 @@ class InjectiveV2PerpetualDerivativeTests(AbstractPerpetualDerivativeTests.Perpe
         return ""
 
     def configure_erroneous_cancelation_response(
-        self, order: InFlightOrder, mock_api: aioresponses, callback: Optional[Callable] = lambda *args, **kwargs: None
+        self, order: InFlightOrder, mock_api: aioresponses, callback: Callable | None = lambda *args, **kwargs: None
     ) -> str:
         transaction_simulation_response = self._msg_exec_simulation_mock_response()
         self.exchange._data_source._query_executor._simulate_transaction_responses.put_nowait(
@@ -634,21 +634,21 @@ class InjectiveV2PerpetualDerivativeTests(AbstractPerpetualDerivativeTests.Perpe
         return ""
 
     def configure_order_not_found_error_cancelation_response(
-        self, order: InFlightOrder, mock_api: aioresponses, callback: Optional[Callable] = lambda *args, **kwargs: None
+        self, order: InFlightOrder, mock_api: aioresponses, callback: Callable | None = lambda *args, **kwargs: None
     ) -> str:
         raise NotImplementedError
 
     def configure_one_successful_one_erroneous_cancel_all_response(
         self, successful_order: InFlightOrder, erroneous_order: InFlightOrder, mock_api: aioresponses
-    ) -> List[str]:
+    ) -> list[str]:
         raise NotImplementedError
 
     def configure_completely_filled_order_status_response(
         self,
         order: InFlightOrder,
         mock_api: aioresponses,
-        callback: Optional[Callable] = lambda *args, **kwargs: None,
-    ) -> List[str]:
+        callback: Callable | None = lambda *args, **kwargs: None,
+    ) -> list[str]:
         self.configure_all_symbols_response(mock_api=mock_api)
         response = self._order_status_request_completely_filled_mock_response(order=order)
         mock_queue = AsyncMock()
@@ -657,8 +657,8 @@ class InjectiveV2PerpetualDerivativeTests(AbstractPerpetualDerivativeTests.Perpe
         return []
 
     def configure_canceled_order_status_response(
-        self, order: InFlightOrder, mock_api: aioresponses, callback: Optional[Callable] = lambda *args, **kwargs: None
-    ) -> Union[str, List[str]]:
+        self, order: InFlightOrder, mock_api: aioresponses, callback: Callable | None = lambda *args, **kwargs: None
+    ) -> str | list[str]:
         self.configure_all_symbols_response(mock_api=mock_api)
 
         self.exchange._data_source._query_executor._spot_trades_responses.put_nowait(
@@ -675,8 +675,8 @@ class InjectiveV2PerpetualDerivativeTests(AbstractPerpetualDerivativeTests.Perpe
         return []
 
     def configure_open_order_status_response(
-        self, order: InFlightOrder, mock_api: aioresponses, callback: Optional[Callable] = lambda *args, **kwargs: None
-    ) -> List[str]:
+        self, order: InFlightOrder, mock_api: aioresponses, callback: Callable | None = lambda *args, **kwargs: None
+    ) -> list[str]:
         self.configure_all_symbols_response(mock_api=mock_api)
 
         self.exchange._data_source._query_executor._derivative_trades_responses.put_nowait(
@@ -690,7 +690,7 @@ class InjectiveV2PerpetualDerivativeTests(AbstractPerpetualDerivativeTests.Perpe
         return []
 
     def configure_http_error_order_status_response(
-        self, order: InFlightOrder, mock_api: aioresponses, callback: Optional[Callable] = lambda *args, **kwargs: None
+        self, order: InFlightOrder, mock_api: aioresponses, callback: Callable | None = lambda *args, **kwargs: None
     ) -> str:
         self.configure_all_symbols_response(mock_api=mock_api)
 
@@ -704,7 +704,7 @@ class InjectiveV2PerpetualDerivativeTests(AbstractPerpetualDerivativeTests.Perpe
         return None
 
     def configure_partially_filled_order_status_response(
-        self, order: InFlightOrder, mock_api: aioresponses, callback: Optional[Callable] = lambda *args, **kwargs: None
+        self, order: InFlightOrder, mock_api: aioresponses, callback: Callable | None = lambda *args, **kwargs: None
     ) -> str:
         self.configure_all_symbols_response(mock_api=mock_api)
         response = self._order_status_request_partially_filled_mock_response(order=order)
@@ -714,8 +714,8 @@ class InjectiveV2PerpetualDerivativeTests(AbstractPerpetualDerivativeTests.Perpe
         return None
 
     def configure_order_not_found_error_order_status_response(
-        self, order: InFlightOrder, mock_api: aioresponses, callback: Optional[Callable] = lambda *args, **kwargs: None
-    ) -> List[str]:
+        self, order: InFlightOrder, mock_api: aioresponses, callback: Callable | None = lambda *args, **kwargs: None
+    ) -> list[str]:
         self.configure_all_symbols_response(mock_api=mock_api)
         response = self._order_status_request_not_found_mock_response(order=order)
         mock_queue = AsyncMock()
@@ -724,7 +724,7 @@ class InjectiveV2PerpetualDerivativeTests(AbstractPerpetualDerivativeTests.Perpe
         return []
 
     def configure_partial_fill_trade_response(
-        self, order: InFlightOrder, mock_api: aioresponses, callback: Optional[Callable] = lambda *args, **kwargs: None
+        self, order: InFlightOrder, mock_api: aioresponses, callback: Callable | None = lambda *args, **kwargs: None
     ) -> str:
         response = self._order_fills_request_partial_fill_mock_response(order=order)
         mock_queue = AsyncMock()
@@ -733,7 +733,7 @@ class InjectiveV2PerpetualDerivativeTests(AbstractPerpetualDerivativeTests.Perpe
         return None
 
     def configure_erroneous_http_fill_trade_response(
-        self, order: InFlightOrder, mock_api: aioresponses, callback: Optional[Callable] = lambda *args, **kwargs: None
+        self, order: InFlightOrder, mock_api: aioresponses, callback: Callable | None = lambda *args, **kwargs: None
     ) -> str:
         mock_queue = AsyncMock()
         mock_queue.get.side_effect = IOError("Test error for trades responses")
@@ -741,7 +741,7 @@ class InjectiveV2PerpetualDerivativeTests(AbstractPerpetualDerivativeTests.Perpe
         return None
 
     def configure_full_fill_trade_response(
-        self, order: InFlightOrder, mock_api: aioresponses, callback: Optional[Callable] = lambda *args, **kwargs: None
+        self, order: InFlightOrder, mock_api: aioresponses, callback: Callable | None = lambda *args, **kwargs: None
     ) -> str:
         response = self._order_fills_request_full_fill_mock_response(order=order)
         mock_queue = AsyncMock()
@@ -940,7 +940,7 @@ class InjectiveV2PerpetualDerivativeTests(AbstractPerpetualDerivativeTests.Perpe
         queue_mock.get.side_effect = Exception("Test error")
         self.exchange._data_source._query_executor._spot_markets_responses = queue_mock
 
-        result: List[str] = await asyncio.wait_for(self.exchange.all_trading_pairs(), timeout=10)
+        result: list[str] = await asyncio.wait_for(self.exchange.all_trading_pairs(), timeout=10)
 
         self.assertEqual(0, len(result))
 
@@ -986,7 +986,7 @@ class InjectiveV2PerpetualDerivativeTests(AbstractPerpetualDerivativeTests.Perpe
         )
         self.exchange._data_source._query_executor._send_transaction_responses = mock_queue
 
-        orders: List[LimitOrder] = self.exchange.batch_order_create(orders_to_create=orders_to_create)
+        orders: list[LimitOrder] = self.exchange.batch_order_create(orders_to_create=orders_to_create)
 
         buy_order_to_create_in_flight = GatewayPerpetualInFlightOrder(
             client_order_id=orders[0].client_order_id,
@@ -1093,7 +1093,7 @@ class InjectiveV2PerpetualDerivativeTests(AbstractPerpetualDerivativeTests.Perpe
             volume=Decimal(str(sell_order_to_create.amount)),
         ).result_price
 
-        orders: List[LimitOrder] = self.exchange.batch_order_create(orders_to_create=orders_to_create)
+        orders: list[LimitOrder] = self.exchange.batch_order_create(orders_to_create=orders_to_create)
 
         buy_order_to_create_in_flight = GatewayPerpetualInFlightOrder(
             client_order_id=orders[0].client_order_id,
@@ -2203,7 +2203,7 @@ class InjectiveV2PerpetualDerivativeTests(AbstractPerpetualDerivativeTests.Perpe
         response = self.latest_prices_request_mock_response
         self.exchange._data_source._query_executor._derivative_trades_responses.put_nowait(response)
 
-        latest_prices: Dict[str, float] = await asyncio.wait_for(
+        latest_prices: dict[str, float] = await asyncio.wait_for(
             self.exchange.get_last_traded_prices(trading_pairs=[self.trading_pair]),
             timeout=1,
         )
@@ -3272,7 +3272,7 @@ class InjectiveV2PerpetualDerivativeTests(AbstractPerpetualDerivativeTests.Perpe
             )
         )
 
-    def _expected_initial_status_dict(self) -> Dict[str, bool]:
+    def _expected_initial_status_dict(self) -> dict[str, bool]:
         status_dict = super()._expected_initial_status_dict()
         status_dict["data_source_initialized"] = False
         return status_dict
@@ -3287,9 +3287,9 @@ class InjectiveV2PerpetualDerivativeTests(AbstractPerpetualDerivativeTests.Perpe
 
     def _configure_balance_response(
         self,
-        response: Dict[str, Any],
+        response: dict[str, Any],
         mock_api: aioresponses,
-        callback: Optional[Callable] = lambda *args, **kwargs: None,
+        callback: Callable | None = lambda *args, **kwargs: None,
     ) -> str:
         self.configure_all_symbols_response(mock_api=mock_api)
         self.exchange._data_source._query_executor._account_portfolio_responses.put_nowait(response)
@@ -3318,21 +3318,21 @@ class InjectiveV2PerpetualDerivativeTests(AbstractPerpetualDerivativeTests.Perpe
             },
         }
 
-    def _order_cancelation_request_successful_mock_response(self, order: InFlightOrder) -> Dict[str, Any]:
+    def _order_cancelation_request_successful_mock_response(self, order: InFlightOrder) -> dict[str, Any]:
         return {
             "txhash": "79DBF373DE9C534EE2DC9D009F32B850DA8D0C73833FAA0FD52C6AE8989EC659",  # noqa: mock
             "rawLog": "[]",
             "code": 0,
         }
 
-    def _order_cancelation_request_erroneous_mock_response(self, order: InFlightOrder) -> Dict[str, Any]:
+    def _order_cancelation_request_erroneous_mock_response(self, order: InFlightOrder) -> dict[str, Any]:
         return {
             "txhash": "79DBF373DE9C534EE2DC9D009F32B850DA8D0C73833FAA0FD52C6AE8989EC659",  # noqa: mock
             "rawLog": "Error",
             "code": 11,
         }
 
-    def _order_status_request_open_mock_response(self, order: GatewayPerpetualInFlightOrder) -> Dict[str, Any]:
+    def _order_status_request_open_mock_response(self, order: GatewayPerpetualInFlightOrder) -> dict[str, Any]:
         return {
             "orders": [
                 {
@@ -3360,7 +3360,7 @@ class InjectiveV2PerpetualDerivativeTests(AbstractPerpetualDerivativeTests.Perpe
 
     def _order_status_request_partially_filled_mock_response(
         self, order: GatewayPerpetualInFlightOrder
-    ) -> Dict[str, Any]:
+    ) -> dict[str, Any]:
         return {
             "orders": [
                 {
@@ -3388,7 +3388,7 @@ class InjectiveV2PerpetualDerivativeTests(AbstractPerpetualDerivativeTests.Perpe
 
     def _order_status_request_completely_filled_mock_response(
         self, order: GatewayPerpetualInFlightOrder
-    ) -> Dict[str, Any]:
+    ) -> dict[str, Any]:
         return {
             "orders": [
                 {
@@ -3414,7 +3414,7 @@ class InjectiveV2PerpetualDerivativeTests(AbstractPerpetualDerivativeTests.Perpe
             "paging": {"total": "1"},
         }
 
-    def _order_status_request_canceled_mock_response(self, order: GatewayPerpetualInFlightOrder) -> Dict[str, Any]:
+    def _order_status_request_canceled_mock_response(self, order: GatewayPerpetualInFlightOrder) -> dict[str, Any]:
         return {
             "orders": [
                 {
@@ -3440,13 +3440,13 @@ class InjectiveV2PerpetualDerivativeTests(AbstractPerpetualDerivativeTests.Perpe
             "paging": {"total": "1"},
         }
 
-    def _order_status_request_not_found_mock_response(self, order: GatewayPerpetualInFlightOrder) -> Dict[str, Any]:
+    def _order_status_request_not_found_mock_response(self, order: GatewayPerpetualInFlightOrder) -> dict[str, Any]:
         return {
             "orders": [],
             "paging": {"total": "0"},
         }
 
-    def _order_fills_request_partial_fill_mock_response(self, order: GatewayPerpetualInFlightOrder) -> Dict[str, Any]:
+    def _order_fills_request_partial_fill_mock_response(self, order: GatewayPerpetualInFlightOrder) -> dict[str, Any]:
         return {
             "trades": [
                 {
@@ -3472,7 +3472,7 @@ class InjectiveV2PerpetualDerivativeTests(AbstractPerpetualDerivativeTests.Perpe
             "paging": {"total": "1", "from": 1, "to": 1},
         }
 
-    def _order_fills_request_full_fill_mock_response(self, order: GatewayPerpetualInFlightOrder) -> Dict[str, Any]:
+    def _order_fills_request_full_fill_mock_response(self, order: GatewayPerpetualInFlightOrder) -> dict[str, Any]:
         return {
             "trades": [
                 {
